f726ca8ec7f43c707211977bdb12710cddd73016
Joshua Limpiado <jianlimpiado@gmail.com>
2021-03-14 18:23:59 +0800

    put the footer inside the body

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,33 +3,30 @@ import WeatherBackground from "./assets/Weather-app_Background-image.png";
 
 const GlobalStyle = createGlobalStyle`
 @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-body {
-  width: 100vw;
-  height: 100vh;
-  background: url(${WeatherBackground});
-  background-size: cover;
-  /* Centering the app */
-  display: grid;
-  justify-content: center;
-  align-items: center;
-}
+  body {
+    height: 100vh;
+    background: url(${WeatherBackground});
+    background-size: cover;
+    /* Centering the app */
+    display: grid;
+    justify-content: center;
+    align-items: center;
 
-body footer {
-  /* position: absolute; */
-  font-size: 1.5rem;
-  font-family: "Poppins", sans-serif;
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  text-align: center;
-  /* height: [footer-height]; */
+    footer {
+    font-size: 1rem;
+    font-family: "Poppins", sans-serif;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    text-align: center;
+  }
 }
 `;
 
