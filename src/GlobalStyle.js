182ffaba80b1e68af122a10a3d9c3f155f405140
Joshua Limpiado <jianlimpiado@gmail.com>
2021-03-15 23:30:13 +0800

    Changed the background and added social icons

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-import WeatherBackground from "./assets/Weather-app_Background-image.png";
 
 const GlobalStyle = createGlobalStyle`
-@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
   * {
     margin: 0;
     padding: 0;
@@ -11,8 +9,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     height: 100vh;
-    background: url(${WeatherBackground});
-    background-size: cover;
+    background: radial-gradient(#0C0C1E,#1B2E3C) no-repeat;
     /* Centering the app */
     display: grid;
     justify-content: center;
@@ -22,10 +19,31 @@ const GlobalStyle = createGlobalStyle`
     font-size: 1rem;
     font-family: "Poppins", sans-serif;
     position: absolute;
+    color: #f3f3f2;
     left: 0;
     right: 0;
     bottom: 0;
     text-align: center;
+  }
+
+  .social-icons{
+    position: fixed;
+    bottom: 40%;
+    left: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+
+    a{
+      text-decoration: none;
+      color: #f3f3f3;
+      font-size: 1.8rem;
+      border-bottom: 1px solid #f3f3f3;
+      transition: color .5s ease-in;
+      &:hover{
+        color: lightskyblue;
+      }
+    }
   }
 }
 `;
