712805314348e1859918e1fd65fc8877522c3cd9
Joshua Limpiado <jianlimpiado@gmail.com>
2021-03-16 16:04:37 +0800

    Minore layout updates for footer and social icons

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -9,7 +9,9 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     height: 100vh;
-    background: radial-gradient(#0C0C1E,#1B2E3C) no-repeat;
+    background: radial-gradient(#0C0C1E,#1B2E3C) no-repeat center center fixed;
+    background-color: #1B2E3C;
+    background-size: cover;
     /* Centering the app */
     display: grid;
     justify-content: center;
@@ -18,20 +20,14 @@ const GlobalStyle = createGlobalStyle`
     footer {
     font-size: 1rem;
     font-family: "Poppins", sans-serif;
-    position: absolute;
     color: #f3f3f2;
-    left: 0;
-    right: 0;
-    bottom: 0;
     text-align: center;
   }
 
   .social-icons{
-    position: fixed;
-    bottom: 40%;
-    left: 10px;
     display: flex;
-    flex-direction: column;
+    margin: 15px 0 10px 0;
+    justify-content: center;
     gap: 25px;
 
     a{
